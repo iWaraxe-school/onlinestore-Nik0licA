32a1a81fc457c54ac45214c4b9cb3139fea46faf
aleksandar.nikolic <aleksandar.nikolic@intradiem.com>
2022-06-08 12:20:28 +0300

    add StoreApp class
    Change-Id: I37c0f738e44503554fd91eb38ca61ed9edc53446

--- store/src/main/java/bg/coherent/store/Store.java
+++ store/src/main/java/bg/coherent/store/Store.java
@@ -3,15 +3,25 @@ package bg.coherent.store;
 import bg.coherent.domain.Category;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Map;
 
 
 
 public class Store {
-       private List<Category> categoryList = new ArrayList<>();
+      // private List<Category> categoryList = new ArrayList<>();
+    Map<Category, Integer> categoryProductsMap;
+    public void fillStore(Map<Category, Integer> categoryProductsMap) {
+        RandomStorePopulator randomStorePopulator = new RandomStorePopulator();
+        for (Map.Entry<Category, Integer> entry : categoryProductsMap.entrySet()) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                Product product = new Product(randomStorePopulator.setName("Food"), randomStorePopulator.setRate(), randomStorePopulator.setPrice());
+                entry.getKey().addProduct(product);
+            }
+        }
+        this.categoryProductsMap = categoryProductsMap;
+    }
 
-
-    public List<Category> getCategoryList() {
-        return categoryList;
+    //public List<Category> getCategoryList() {
+    //   return categoryList;
     }
 }
