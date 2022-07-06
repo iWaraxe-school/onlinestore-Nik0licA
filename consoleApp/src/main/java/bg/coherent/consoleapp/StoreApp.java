76d238fa264a5da079adb2373addb8349ecc8711
aleksandar.nikolic <aleksandar.nikolic@intradiem.com>
2022-07-06 11:55:35 +0300

    Adding xml parser
    Change-Id: Ice9f642d1511b7dcb3cb2a4b1f72603e2ad0278f

--- consoleApp/src/main/java/bg/coherent/consoleapp/StoreApp.java
+++ consoleApp/src/main/java/bg/coherent/consoleapp/StoreApp.java
@@ -2,13 +2,14 @@ package bg.coherent.consoleapp;
 
 
 import bg.coherent.store.Store;
+import bg.coherent.store.XmlParser;
 
 public class StoreApp {
     public static void main(String[] args) {
 
         Store store = new Store();
         store.printStore();
-
+        System.out.println(XmlParser.getConfig());
     }
 
 }
