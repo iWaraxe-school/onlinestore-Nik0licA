13d8c86bd3ca2dd24001607d93b87de88e8fce80
aleksandar.nikolic <aleksandar.nikolic@intradiem.com>
2022-08-10 12:09:57 +0300

    Adding interaction and sorting of top five products
    Change-Id: Ia9bc727a3d9dc1f7a561776e2ae83f27599205dc

--- consoleApp/src/main/java/bg/coherent/consoleapp/StoreApp.java
+++ consoleApp/src/main/java/bg/coherent/consoleapp/StoreApp.java
@@ -5,6 +5,10 @@ import bg.coherent.store.ProductComparator;
 import bg.coherent.store.Store;
 import bg.coherent.store.XmlParser;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class StoreApp {
     public static void main(String[] args) {
 
@@ -12,7 +16,36 @@ public class StoreApp {
         store.printStore();
         System.out.println(XmlParser.getConfig());
         System.out.println(ProductComparator.sortedProducs(store));
+
+        try {
+
+            Scanner scanner = new Scanner(System.in);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            Boolean flag = true;
+            while (flag) {
+                System.out.println("Enter one of following commands sort/top/quit:");
+                String command = reader.readLine();
+                System.out.println("Your command is : " + command);
+
+                switch (command) {
+                    case "sort":
+                        System.out.println("Sorted products list:");
+                        System.out.println(ProductComparator.sortedProducs(store));
+                        break;
+                    case "top":
+                        System.out.println("Top 5 store products are:");
+                        System.out.println(ProductComparator.topFive(store));
+                        break;
+                    case "quit":
+                        flag = false;
+                        break;
+                    default:
+                        System.out.println("Command is not supported, try again");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: exception trown " + e.getMessage());
+        }
     }
-
-
 }
--- store/src/main/java/bg/coherent/store/ProductComparator.java
+++ store/src/main/java/bg/coherent/store/ProductComparator.java
@@ -8,37 +8,45 @@ import java.util.*;
 public class ProductComparator {
 
 
-    public static List<Product> sortedProducs(Store store){
-
-        List<Product> allProducts = store.getAllProducts();
-        List<String> allKeys = new ArrayList<>();
-        List<String> allValues = new ArrayList<>();
-        for (Map.Entry<String, String> entry: XmlParser.getConfig().entrySet()){
-            allKeys.add(entry.getKey());
-            allValues.add(entry.getValue());
+    public static List<Product> sortedProducs(Store store) {
+        return sortProducts(XmlParser.getConfig(), store.getAllProducts());
+    }
+    public static List<Product> topFive (Store store){
+        HashMap<String, String> priceRule = new HashMap<>();
+        priceRule.put("price", "DESC");
+        List<Product> allProducts = sortProducts(priceRule,store.getAllProducts());
+        if(allProducts.size()>5) {
+            return allProducts.subList(0, 5);
         }
-        for (int i = allKeys.size() -1; i>= 0; i--){
-            switch(allKeys.get(i)){
-                    case "name":
-                        if(allValues.get(i).equals("ASC")){
-                            allProducts.sort(Comparator.comparing(Product::getName));
-                        } else{allProducts.sort(Comparator.comparing(Product::getName).reversed());}
-                        break;
-                    case "price":
-                        if(allValues.get(i).equals("ASC")){
-                            allProducts.sort(Comparator.comparing(Product::getPrice));
-                        } else{allProducts.sort(Comparator.comparing(Product::getPrice).reversed());}
-                        break;
-                    case "rate":
-                        if(allValues.get(i).equals("ASC")){
-                            allProducts.sort(Comparator.comparing(Product::getRate));
-                        } else{allProducts.sort(Comparator.comparing(Product::getRate).reversed());}
-                        break;
+        else{
+            return allProducts;
+        }
 
+    }
+    public static List<Product> sortProducts(Map<String, String> rules, List<Product> allProducts){
+        for (Map.Entry<String, String> rule :rules.entrySet()){
+            switch(rule.getKey()){
+                case "name":
+                    if(rule.getValue().equals("ASC")){
+                        allProducts.sort(Comparator.comparing(Product::getName));
+                    } else{allProducts.sort(Comparator.comparing(Product::getName).reversed());}
+                    break;
+                case "price":
+                    if(rule.getValue().equals("ASC")){
+                        allProducts.sort(Comparator.comparing(Product::getPrice));
+                    } else{allProducts.sort(Comparator.comparing(Product::getPrice).reversed());}
+                    break;
+                case "rate":
+                    if(rule.getValue().equals("ASC")){
+                        allProducts.sort(Comparator.comparing(Product::getRate));
+                    } else{allProducts.sort(Comparator.comparing(Product::getRate).reversed());}
+                    break;
             }
         }
             return allProducts;
-    }
+        }
+
 
     }
+
 
